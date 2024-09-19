f911fe7d0581d901af9c603dc42756a10dfb2af3
FaithO3321 <faithomondi1331@gmail>
2024-09-19 14:04:24 -0700

    Defines how models are displayed in the Django admin interface

--- biodiversity_quiz_project/quiz_app/admin.py
+++ biodiversity_quiz_project/quiz_app/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
 from .models import Category, Quiz, Question, Choice, QuizResult
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 4
 
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
+
 
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 5
 
+
 class QuizAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+
 
 admin.site.register(Category)
 admin.site.register(Quiz, QuizAdmin)
