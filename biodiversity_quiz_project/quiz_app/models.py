5b801d2c5aabdbe831c98ddb591af8f1bfb6c779
FaithO3321 <faithomondi1331@gmail>
2024-09-19 14:12:14 -0700

    defines database models using Django ORM

--- biodiversity_quiz_project/quiz_app/models.py
+++ biodiversity_quiz_project/quiz_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
@@ -15,12 +17,14 @@ class Quiz(models.Model):
     def __str__(self):
         return self.title
 
+
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     text = models.TextField()
 
     def __str__(self):
         return self.text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -29,6 +33,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.text
+
 
 class QuizResult(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
