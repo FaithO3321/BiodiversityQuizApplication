f6a6f9b1304e9d51e9c850e4fbbccecfe429cdb6
FaithO3321 <faithomondi1331@gmail>
2024-09-19 14:13:40 -0700

    defines how model instances are converted to JSON

--- biodiversity_quiz_project/quiz_app/serializers.py
+++ biodiversity_quiz_project/quiz_app/serializers.py
@@ -2,20 +2,24 @@ from rest_framework import serializers
 from .models import Category, Quiz, Question, Choice, QuizResult
 from django.contrib.auth.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ['id', 'text']
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     choices = ChoiceSerializer(many=True, read_only=True)
@@ -24,12 +28,14 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ['id', 'text', 'choices']
 
+
 class QuizSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Quiz
         fields = ['id', 'title', 'category', 'questions']
+
 
 class QuizResultSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
