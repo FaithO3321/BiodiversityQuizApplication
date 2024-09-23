82e66e013eaaeffe3ddbbeaf890ab0a674e1cc18
FaithO3321 <faithomondi1331@gmail>
2024-09-23 01:57:23 -0700

    handling logic for endpoints

--- biodiversity_quiz_project/quiz_app/views.py
+++ biodiversity_quiz_project/quiz_app/views.py
@@ -17,7 +17,18 @@ from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.pagination import PageNumberPagination
+from django.conf import settings
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework import viewsets
 
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @method_decorator(cache_page(settings.CACHE_TTL))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
